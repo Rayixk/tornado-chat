972e32e7c832f94821e7c667a8d71b2f189f26f3
jackChen8850 <472093743@qq.com>
2017-11-17 17:32:28 +0800

    WebSocket 第一步

--- chatdemo.py
+++ chatdemo.py
@@ -1,18 +1,23 @@
 # coding:utf-8
 # Created by yang
+import uuid
 
+import datetime
 import tornado.ioloop
 import tornado.options
 import tornado.web
 import os.path
+import tornado.websocket
+import logging
 from tornado.options import define, options
 
-define("port", default=8887, help="run on the given port", type=int)
+define("port", default=8883, help="run on the given port", type=int)
 
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/", MainHandler),
+            (r"/chatsocket", ChatSocketHandler),
         ]
         settings = dict(
             cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
@@ -28,6 +33,43 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
+class ChatSocketHandler(tornado.websocket.WebSocketHandler):
+    waiters = set()
+
+    def open(self):
+        ChatSocketHandler.waiters.add(self)
+
+    def on_close(self):
+        ChatSocketHandler.waiters.remove(self)
+
+    def on_message(self, message):
+        logging.info("got message %r", message)
+        parsed = tornado.escape.json_decode(message)
+        logging.info("got parsed %r", parsed)
+        # self.username = parsed["username"]
+        self.username = parsed.get("username")
+        chat = {
+            "id": str(uuid.uuid4()),
+            "body": parsed.get("body"),
+            "type": "message",
+            # "client_id": self.client_id,
+            "username": self.username,
+            "datetime": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            }
+        chat["html"] = tornado.escape.to_basestring(
+            self.render_string("message.html", message=chat))
+        ChatSocketHandler.send_updates(chat)
+
+    @classmethod
+    def send_updates(cls, chat): #向所有客户端发送聊天信息
+        logging.info("sending message to %d waiters", len(cls.waiters))
+        for waiter in cls.waiters:
+            try:
+                waiter.write_message(chat)
+            except:
+                logging.error("Error sending message", exc_info=True)
+
+
 def main():
     tornado.options.parse_command_line()
     app=Application()
@@ -36,6 +78,8 @@ def main():
         tornado.ioloop.IOLoop.current().start()
     except KeyboardInterrupt:
         tornado.ioloop.IOLoop.current().stop()
+    except Exception:
+        pass
 
 if __name__ == '__main__':
     main()
--- static/chat.js
+++ static/chat.js
@@ -1,3 +1,71 @@
 /**
  * Created by Administrator on 2017/11/17.
  */
+
+$(document).ready(function () {
+    if (!window.console) window.console = {};
+    if (!window.console.log) window.console.log = function () {
+    };
+
+    $("#messageform").live("submit", function () {
+        newMessage($(this));
+        return false;
+    });
+    $("#messageform").live("keypress", function (e) {
+        if (e.keyCode == 13) {
+            newMessage($(this));
+            return false;
+        }
+    });
+    $("#message").select();
+    updater.start();
+});
+
+function newMessage(form) {
+    var message = form.formToDict();
+    console.log(JSON.stringify(message))
+    updater.socket.send(JSON.stringify(message));
+    form.find("#message").val("").select();
+}
+
+jQuery.fn.formToDict = function () {
+    var fields = this.serializeArray();
+    var json = {}
+    for (var i = 0; i < fields.length; i++) {
+        json[fields[i].name] = fields[i].value;
+    }
+    if (json.next) delete json.next;
+    return json;
+};
+
+var updater = {
+    socket: null,
+
+    start: function () {
+        var url = "ws://" + location.host + "/chatsocket";
+        updater.socket = new WebSocket(url);
+        console.log("1111111111111")
+        updater.socket.onmessage = function (event) {
+            console.log("2222222222222")
+            updater.showMessage(JSON.parse(event.data));
+        }
+    },
+
+    showMessage: function (message) {
+        var node = $(message.html)
+        node.hide()
+        $("#inbox").append(node)
+        node.slideDown()
+        // del(message.client_id);
+        // if (message.type != "offline") {
+        //     add(message.client_id, message.username);
+        //     if (message.body == "") return;
+        //     var existing = $("#m" + message.id);
+        //     if (existing.length > 0) return;
+        //     var node = $(message.html);
+        //     node.hide();
+        //     $("#inbox").append(node);
+        //     node.slideDown();
+        // }
+    }
+};
